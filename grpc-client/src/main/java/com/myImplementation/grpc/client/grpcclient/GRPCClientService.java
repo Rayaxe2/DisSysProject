3eed3f481e46393502badd34ce889bd4064c4d4e
Rayaxe2 <rayaxemailbox@gmail.com>
2021-04-02 23:14:33 +0100

    All Working

--- grpc-client/src/main/java/com/myImplementation/grpc/client/grpcclient/GRPCClientService.java
+++ grpc-client/src/main/java/com/myImplementation/grpc/client/grpcclient/GRPCClientService.java
@@ -32,7 +32,7 @@ public class GRPCClientService {
 			ManagedChannel channel = ManagedChannelBuilder.forAddress(serverIPs[serverIndex], 9090).usePlaintext().build();
 
 			//For Debugging
-			System.out.println(channel.getState(false).toString());
+			System.out.println("State: " + channel.getState(false).toString());
 
 			//We create a stub and pass the channel in as a parameter to link it to the server
 			MatrixMultServiceGrpc.MatrixMultServiceBlockingStub stub = MatrixMultServiceGrpc.newBlockingStub(channel);
@@ -280,7 +280,7 @@ public class GRPCClientService {
 		//In an attempt to find a server that is active and accepting connection and messages from the client
 		catch (IllegalArgumentException e){
 			//If the user attempted to connect to every server in the list of IPs and failed, then an error is returned to the rest controlled
-			if(serverIndex < 7) {
+			if(serverIndex < 8) {
 				return matrixMultiplicationOperation(mA, mB, dimentions, deadline, serverIndex + 1);
 			}
 			else {
@@ -288,7 +288,7 @@ public class GRPCClientService {
 			}
 		}
 		catch (io.grpc.StatusRuntimeException e) {
-			if(serverIndex < 7) {
+			if(serverIndex < 8) {
 				return matrixMultiplicationOperation(mA, mB, dimentions, deadline, serverIndex + 1);
 			}
 			else {
@@ -457,7 +457,7 @@ public class GRPCClientService {
 		//In an attempt to find a server that is active and accepting connection and messages from the client
 		catch (IllegalArgumentException e){
 				//If the user attempted to connect to every server in the list of IPs and failed, then an error is returned to the rest controlled
-				if(serverIndex < 7) {
+				if(serverIndex < 8) {
 					return matrixAdditionOperation(mA, mB, dimentions, deadline, serverIndex + 1);
 				}
 				else {
@@ -465,7 +465,7 @@ public class GRPCClientService {
 				}
 			}
 		catch (io.grpc.StatusRuntimeException e) {
-				if(serverIndex < 7) {
+				if(serverIndex < 8) {
 					return matrixAdditionOperation(mA, mB, dimentions, deadline, serverIndex + 1);
 				}
 				else {
